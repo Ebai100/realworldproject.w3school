fdc064e7eeebdd822b4e61efb3d01966f743ae2f
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-09-29 21:45:45 -0700

    real project from w3school

---
+++ realProject1/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //student data
+        String studentName= "Ebai Ebai";
+        int studentId = 20;
+        int studentAge = 28;
+        float studentFee = 78.25f;
+        char studentGrade = 'A';
+        System.out.println("studentName:" + studentName);
+        System.out.println("studentId:" + studentId);
+        System.out.println("studentAge:" + studentAge);
+        System.out.println("studentFee:" + studentFee);
+        System.out.println("studentGrade:" + studentGrade);
+    }
+}
