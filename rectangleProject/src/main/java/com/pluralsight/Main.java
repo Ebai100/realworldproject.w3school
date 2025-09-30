9795abe259daa1665d818d203edd26ecce672970
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-09-29 21:53:36 -0700

    calculating the area of a rectangle

---
+++ rectangleProject/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Create integer variables
+        int length = 4;
+        int width = 6;
+        int area;
+        area =length*width;
+        System.out.println("the area is:" + area);
+
+    }
+}
