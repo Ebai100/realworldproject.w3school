911243629c621ff0563c23a84eb21dfd754a6d1d
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-09-30 09:06:45 -0700

    for loop exercise

---
+++ demoProject/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 2 ; i++){
+            System.out.printf("%d. I will be a good developer.%n", i);
+        }
+
+        for (int i = 81; i > 0; i--) {
+            System.out.printf("%d more days left of Year Up Code Academy.%n", i);
+        }
+    }
+}
